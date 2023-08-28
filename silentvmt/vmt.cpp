1bdd18bc55f65699721b6eb3be42b37188deb390
nt <99148819+ntos322@users.noreply.github.com>
2023-08-28 17:59:22 +0200

    mem leak fix=

--- silentvmt/vmt.cpp
+++ silentvmt/vmt.cpp
@@ -1,80 +1,42 @@
-#pragma once
-#include <string>
-#include <unordered_map>
+#include "vmt.hpp"
 
-/* for throw */
-#include <stdexcept>
+silent_vmt::silent_vmt(std::uintptr_t* ptr, size_t sz) {
+	if (ptr == nullptr)
+		throw std::invalid_argument("ptr cannot be null bru");
 
-/* for std::function */
-#include <functional>
+	/* store the original vtable */
+	m_originalTable = *(std::uintptr_t**)ptr;
 
-class silent_vmt {
-private:
+	/* i wanted to do this just incase if someone wanted to manually specify the vmt size */
+	if (!sz)
+		sz = get_count();
 
-	struct {
+	/* alloc a new "copy" of the vtable, this is where the "shadowing" starts (+ 1 because we need somewhere to store the ptr to the orig vmt) */
+    m_copiedTable = new std::uintptr_t[sz + 1];
 
-	/* a map to store the original functions
-         * key: the index of the function in the vmt
-         * value: addr of the orig function
-        */
-	std::unordered_map<int, std::uintptr_t>m_originalFunctions;
+	/* copy the original vmt into the new vtable */
+	std::memcpy(&m_copiedTable[1], m_originalTable, sz * sizeof(std::uintptr_t));
 
-        /* a ptr to the original virtual method table (vmt) instance */
-        std::uintptr_t* m_originalTable;
+	/* update the instance's vmt ptr to our new "shadow" / "copy" */
+	*ptr = (std::uintptr_t)&m_copiedTable[1];
 
-        /* the "shadowed" table */
-        std::uintptr_t* m_copiedTable;
+}
 
-	};
+auto silent_vmt::hook(int idx, std::uintptr_t hook) -> bool {
+	auto original_function = m_originalTable[idx];
+	if (!original_function)
+		return false;
 
-public:
+	m_originalFunctions.insert( { idx, original_function } );
+	m_copiedTable[idx + 1] = hook;
 
-    /*
-     * constructor for the silent_vmt class.
-     *
-     * @param ptr: a ptr to the instance of the class whose vmt is to be hooked.
-     * this is used to create and set up the shadow vmt for the specified instance ptr
-    */
-    silent_vmt(std::uintptr_t* ptr, size_t sz = 0);
+	return true;
+}
 
+auto silent_vmt::get_count() -> int {
+	int retval = 0;
+	while (m_originalTable[retval] != 0)
+		retval++;
 
-    /* deconstructor for the silent_vmt class. */
-    ~silent_vmt() {
-        m_originalFunctions.clear();
-        delete[] m_copiedTable;
-    }
-
-    /*
-     * get the number of functions in the virtual table.
-     * 
-     * this func counts and returns the num of valid function ptrs
-     * @return int the number of functions in the vmt
-    */
-    auto get_count() -> int;
-
-    /*
-     * hooks a func in the vmt at the specified index
-     * @return true if successful, false otherwise
-    */
-    auto hook(int idx, std::uintptr_t hook) -> bool;
-
-    /*
-     * if you for some reason dont want to directly call the function with "call_original", you can get the address without calling the actual function
-     * @return the orig func addr
-    */
-    auto get_original(int idx) -> std::uintptr_t {
-        return m_originalFunctions[idx];
-    }
-
-    /*
-     * call the original function by index
-     * @return the original functions retval
-    */
-    template <typename T, typename ...arguments>
-    auto call_original(int idx, arguments... args) -> T {
-        auto addr = m_originalFunctions[idx];
-        std::function<T(arguments...)> original_function = reinterpret_cast<T(*)(arguments...)>(addr);
-        return original_function(args...);
-    }
-
-};
+	return retval;
+}
